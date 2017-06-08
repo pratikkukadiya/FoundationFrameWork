8861403bd0a1b6a2d06d20eb469f02b042e65d16
pratikkukadiya <pratik.ios.devloper@gmail.com>
2017-06-08 21:23:58 +0530

    first

---
+++ NSNumber/NSNumber/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  NSNumber
+//
+//  Created by Pratik kukadiya on 6/8/17.
+//  Copyright © 2017 Pratik kukadiya. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
